8d2ef99ce610bea03c4003a71c6253b01a241784
Nika Avramchuk <nika.avramchuk@gmail.com>
2020-12-14 23:58:41 +0100

    create Square class with parameters x and y

---
+++ src/main/java/Square.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class Square extends JButton {
+    private final int x;
+    private final int y;
+
+    public Square(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    private static class Combination {
+        Square firstRowFirstColumn = new Square(1,1);
+        Square firstRowSecondColumn = new Square(1,2);
+        Square firstRowThirdColumn = new Square(1,3);
+        Square secondRowFirstColumn = new Square(2,1);
+        Square secondRowSecondColumn = new Square(2,2);
+        Square secondRowThirdColumn = new Square(2,3);
+        Square thirdRowFirstColumn = new Square(3,1);
+        Square thirdRowSecondColumn = new Square(3,2);
+        Square thirdRowThirdColumn = new Square(3,3);
+
+        Square[] firstRow = {firstRowFirstColumn,firstRowSecondColumn,firstRowThirdColumn};
+        Square[] secondRow = {secondRowFirstColumn,secondRowSecondColumn,secondRowThirdColumn};
+        Square[] thirdRow = {thirdRowFirstColumn,thirdRowSecondColumn,thirdRowThirdColumn};
+
+        Square[] firstColumn = {firstRowFirstColumn,secondRowFirstColumn,thirdRowFirstColumn};
+        Square[] secondColumn = {firstRowSecondColumn,secondRowSecondColumn,thirdRowSecondColumn};
+        Square[] thirdColumn = {firstRowThirdColumn,secondRowThirdColumn,thirdRowThirdColumn};
+
+        Square[] rightCross = {firstRowFirstColumn, secondRowSecondColumn, thirdRowThirdColumn};
+        Square[] leftCross = {thirdRowFirstColumn, secondRowSecondColumn, firstRowThirdColumn};
+
+
+    }
+
+}
