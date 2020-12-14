4aa5dda133cba1e2a565577d83649b7db47a16d2
Nika Avramchuk <nika.avramchuk@gmail.com>
2020-12-15 00:28:49 +0100

    put all squares button on board

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -1,6 +1,24 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Panel extends JPanel {
+    Game game = new Game();
     public Panel() {
         setLayout(null);
-    }}
+        locateAllSquaresonBoard(game.allSquares);
+    }
+
+    public void locateAllSquaresonBoard (ArrayList<Square> allSquares) {
+        int horizontal;
+        int vertical;
+
+        for(Square square: allSquares) {
+            horizontal=30+(SquareSize.WIDTH.value*square.x);
+            vertical=30+(SquareSize.HEIGHT.value*square.y);
+            square.setBounds(horizontal,vertical,SquareSize.WIDTH.value,SquareSize.HEIGHT.value);
+            add(square);
+        }
+
+    }
+
+}
