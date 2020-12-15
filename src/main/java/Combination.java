29091c6777741fba6ed2189fa0f11fe9e3293480
Nika Avramchuk <nika.avramchuk@gmail.com>
2020-12-15 02:03:08 +0100

    create class with all potential winning combinations

---
+++ src/main/java/Combination.java
@@ -0,0 +1,24 @@
+public class Combination {
+    static Square firstRowFirstColumn = new Square(1,1);
+    static Square firstRowSecondColumn = new Square(1,2);
+    static Square firstRowThirdColumn = new Square(1,3);
+    static Square secondRowFirstColumn = new Square(2,1);
+    static Square secondRowSecondColumn = new Square(2,2);
+    static Square secondRowThirdColumn = new Square(2,3);
+    static Square thirdRowFirstColumn = new Square(3,1);
+    static Square thirdRowSecondColumn = new Square(3,2);
+    static Square thirdRowThirdColumn = new Square(3,3);
+
+    static Square[] firstRow = {firstRowFirstColumn,firstRowSecondColumn,firstRowThirdColumn};
+    static Square[] secondRow = {secondRowFirstColumn,secondRowSecondColumn,secondRowThirdColumn};
+    static Square[] thirdRow = {thirdRowFirstColumn,thirdRowSecondColumn,thirdRowThirdColumn};
+
+    static Square[] firstColumn = {firstRowFirstColumn,secondRowFirstColumn,thirdRowFirstColumn};
+    static Square[] secondColumn = {firstRowSecondColumn,secondRowSecondColumn,thirdRowSecondColumn};
+    static Square[] thirdColumn = {firstRowThirdColumn,secondRowThirdColumn,thirdRowThirdColumn};
+
+    static Square[] rightCross = {firstRowFirstColumn, secondRowSecondColumn, thirdRowThirdColumn};
+    static Square[] leftCross = {thirdRowFirstColumn, secondRowSecondColumn, firstRowThirdColumn};
+
+
+}
